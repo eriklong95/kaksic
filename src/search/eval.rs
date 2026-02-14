e2dc9fae9357c1119e480a319c976bb43f4b311d
Erik Lange <eriklange.cph@proton.me>
2026-02-14 17:54:03 +0100

    evaluates position

--- src/search/eval.rs
+++ src/search/eval.rs
@@ -1,8 +1,40 @@
-use shakmaty::{ByRole, Chess, Position};
+use shakmaty::{ByRole, Chess, Position, Square};
 
 /// Evaluate the "value" of the position for the player who is about to move
 pub fn eval(position: &Chess) -> i32 {
-    eval_material(position)
+    eval_material(position) + eval_position(position) + eval_checkmate(position)
+}
+
+fn eval_checkmate(position: &Chess) -> i32 {
+    if position.is_checkmate() {
+        return i32::MIN + 1;
+    }
+    return 0;
+}
+
+fn eval_position(position: &Chess) -> i32 {
+    let mut to_move_pieces_in_center = 0;
+
+    let mut other_pieces_in_center = 0;
+
+    for (sq, pc) in position.board() {
+        if is_center(sq) {
+            if pc.color == position.turn() {
+                to_move_pieces_in_center += 1;
+            } else {
+                other_pieces_in_center += 1;
+            }
+        }
+    }
+
+    return (to_move_pieces_in_center - other_pieces_in_center) * 100;
+}
+
+fn is_center(sq: Square) -> bool {
+    sq.file().ge(&shakmaty::File::C)
+        && sq.file().le(&shakmaty::File::F)
+        && sq.rank().ge(&shakmaty::Rank::Third)
+        && sq.rank().le(&shakmaty::Rank::Sixth)
 }
 
 fn eval_material(position: &Chess) -> i32 {
